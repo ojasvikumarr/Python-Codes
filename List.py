840db390fabcc89b978dd7ac556ec77820361b14
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-10-15 18:47:33 +0530

    First commit

---
+++ List.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello, World!")
+list = ["Hello" , "World"]
+print(list)
+for i in range(10):
+    print(i)
+for items in list :
+    print(items)
+ls = []
+for i in range(10):
+    ls.append(i)
+print(ls)
+
+print(ls[:9])
---
+++ try.py
@@ -0,0 +1,7 @@
+print("Hello!")
+n = input("Enter the Price of the product : ")
+answer = 1.5*int(n)
+print("The Price you need to pay will be : ", answer)
+
+for i in range(1 , answer):
+    print(i, " ")
