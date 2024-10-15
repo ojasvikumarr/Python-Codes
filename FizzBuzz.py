423b509664446f139a6f14f398dca1b317ba6eeb
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-10-15 18:55:53 +0530

    FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+for number in range(0 , 51):
+    if number % 5 == 0 and number % 3 == 0 :
+        print(number ,"FizzBuzz")
+        continue
+    else :
+        if number % 3 == 0 :
+            print(number , "Fizz")
+            continue
+        else :
+            if number % 5 == 0 : 
+                print(number , "Buzz")
+                continue
+    print(number)
