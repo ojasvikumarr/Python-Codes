086d014d45a06b418dbc9e7130568516c3f762ec
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-10-18 15:29:34 +0530

    Leap Year

---
+++ Week04/LeapYearOrNot.py
@@ -0,0 +1,12 @@
+while(True):
+    year = input("Enter the year : ")
+    year = int(year)
+    if(year % 400 == 0) :
+        print("This is a Leap Year!!")
+    elif(year % 100 == 0):
+        print("This is not a Leap Year!!")
+    elif(year % 4 == 0):
+        print("This is a Leap Year!!")
+    else:
+        print("This is not a Leap Year!!")
+
